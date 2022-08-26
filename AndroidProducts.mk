5d1437778f714b2c02363d7e49c0d1017ab2d35c
worldbreakerinmortal <56098906+worldbreakerinmortal@users.noreply.github.com>
2022-08-25 22:04:18 -0500

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_YT-X705F.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_YT-X705F-user \
+    omni_YT-X705F-userdebug \
+    omni_YT-X705F-eng
---
+++ omni_YT-X705F.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from YT-X705F device
+$(call inherit-product, device/lenovo/YT-X705F/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := YT-X705F
+PRODUCT_NAME := omni_YT-X705F
+PRODUCT_BRAND := Lenovo
+PRODUCT_MODEL := Lenovo YT-X705F
+PRODUCT_MANUFACTURER := lenovo
